4ff16a2a2fb0a30c4988eb38fe64cc44e2be3cbb
LeanKhan <eslean20@gmail.com>
2020-06-21 15:14:08 +0100

    add forgot-password func

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -2,27 +2,80 @@
   <form ref="form" @submit.prevent="login">
     <v-card>
       <v-card-text>
-        <v-text-field
-          required
-          type="text"
-          label="Username"
-          :disabled="loading"
-          color="green"
-          v-model="form.Username"
-        ></v-text-field>
+        <template v-if="!showForgotSection">
+          <v-subheader>Login to FSPro</v-subheader>
 
-        <v-text-field
-          required
-          type="text"
-          label="Password"
-          :disabled="loading"
-          color="green"
-          v-model="form.Password"
-        ></v-text-field>
+          <v-text-field
+            required
+            type="text"
+            label="Username"
+            :disabled="loading"
+            :loading="loading"
+            color="green"
+            v-model="form.Username"
+          ></v-text-field>
+
+          <v-text-field
+            required
+            type="text"
+            label="Password"
+            :disabled="loading"
+            :loading="loading"
+            color="green"
+            v-model="form.Password"
+          ></v-text-field>
+        </template>
+
+        <template v-else>
+          <v-subheader>Change Password</v-subheader>
+
+          <v-text-field
+            required
+            type="text"
+            label="Username"
+            :disabled="loading"
+            :loading="loading"
+            color="pink"
+            v-model="newForm.Username"
+          ></v-text-field>
+
+          <v-text-field
+            required
+            type="text"
+            label="New Password"
+            :disabled="loading"
+            :loading="loading"
+            color="pink"
+            v-model="newForm.NewPassword"
+          ></v-text-field>
+        </template>
+        <!-- Forgot Password -->
+        <div>
+          Forgot your password?
+          <v-btn outlined depressed @click="showForgot">
+            Yup, ama fish eater
+          </v-btn>
+        </div>
       </v-card-text>
       <v-card-actions>
-        <v-btn color="green darken-2" @click="login" block :loading="loading">
+        <v-btn
+          v-if="!showForgotSection"
+          color="green darken-2"
+          @click="login"
+          block
+          :loading="loading"
+        >
           Login
+        </v-btn>
+
+        <v-btn
+          v-else
+          color="pink darken-2"
+          @click="submitNewPassword"
+          block
+          :loading="loading"
+        >
+          Change Password
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -38,7 +91,14 @@ export default class Login extends Vue {
     Password: '',
   };
 
+  private newForm = {
+    Username: '',
+    NewPassword: '',
+  };
+
   public loading = false;
+
+  public showForgotSection = false;
 
   private login() {
     this.loading = true;
@@ -72,6 +132,41 @@ export default class Login extends Vue {
       .finally(() => {
         this.loading = false;
       });
+  }
+
+  private submitNewPassword() {
+    this.loading = true;
+    this.$axios
+      .post('/users/change-password', this.newForm, { withCredentials: true })
+      .then(response => {
+        console.log('Response => ', response.data);
+        if (response.data.success) {
+          this.$store.dispatch('SET_USER', {
+            username: response.data.payload.Username,
+            userID: response.data.payload._id,
+            clubs: response.data.payload.Clubs,
+            session: response.data.payload.Session,
+            isAdmin: response.data.payload.isAdmin,
+            avatar: response.data.payload.Avatar,
+            fullname: response.data.payload.FullName,
+          });
+        }
+
+        this.$socket.client.emit('authenticate');
+
+        this.$router.push('/u');
+      })
+      .catch(response => {
+        console.log('Error changing password in! ', response.data);
+      })
+      .finally(() => {
+        this.showForgotSection = false;
+        this.loading = false;
+      });
+  }
+
+  private showForgot() {
+    this.showForgotSection = true;
   }
 }
 </script>
