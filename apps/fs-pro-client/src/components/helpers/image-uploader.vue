5c545558c8898075277a1c9f7ca3cd3cb55b4ca5
Emmanuel Segun-Lean <34924520+LeanKhan@users.noreply.github.com>
2025-09-19 22:45:05 -0500

    feat: update image-uploader to vue3

--- apps/fs-pro-client/src/components/helpers/image-uploader.vue
+++ apps/fs-pro-client/src/components/helpers/image-uploader.vue
@@ -74,151 +74,151 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator';
-// import { Club } from '@/interfaces/club';
-// import { apiUrl } from '@/store';
-@Component({})
-export default class ImageUploader extends Vue {
-  @Prop({
-    required: false,
-    default: () => ({
-      height: '',
-      width: '',
-      style: '',
-      src: '',
-      contain: false,
-    }),
-  })
-  readonly previewImage!: {
-    height: string;
-    width: string;
-    style: string;
-    src: string;
-    contain: boolean;
-  };
+<script setup lang="ts">
+import { ref, computed, getCurrentInstance } from 'vue';
+import { useStore } from '@/store';
 
-  @Prop({
-    required: false,
-    default: () => ({
-      height: '300px',
-      width: '245px',
-      class: '',
-      style: '',
-    }),
-  })
-  readonly cardSheet!: {};
+interface PreviewImage {
+  height: string;
+  width: string;
+  style: string;
+  src: string;
+  contain: boolean;
+}
 
-  @Prop({
-    required: false,
-    default: () => ({
-      height: '300px',
-      width: '245px',
-      class: '',
-      style: '',
-      title: 'Upload Image',
-    }),
-  })
-  readonly card!: {};
+interface CardSheet {
+  height: string;
+  width: string;
+  class: string;
+  style: string;
+}
 
-  @Prop({ required: false, default: true }) readonly compress!: boolean;
+interface Card {
+  height: string;
+  width: string;
+  class: string;
+  style: string;
+  title: string;
+}
 
-  @Prop({ required: true, type: String }) readonly filePath!: string;
-  @Prop({ required: true, type: String }) readonly fileName!: string;
+interface Props {
+  previewImage?: PreviewImage;
+  cardSheet?: CardSheet;
+  card?: Card;
+  compress?: boolean;
+  filePath: string;
+  fileName: string;
+}
 
-  private image: File | null = null;
-  private uploadedImageSrc: any = '';
-  private uploading = false;
+const props = withDefaults(defineProps<Props>(), {
+  previewImage: () => ({
+    height: '',
+    width: '',
+    style: '',
+    src: '',
+    contain: false,
+  }),
+  cardSheet: () => ({
+    height: '300px',
+    width: '245px',
+    class: '',
+    style: '',
+  }),
+  card: () => ({
+    height: '300px',
+    width: '245px',
+    class: '',
+    style: '',
+    title: 'Upload Image',
+  }),
+  compress: true,
+});
 
-  get uploadedImage() {
-    if(this.uploadedImageSrc){
-    return this.uploadedImageSrc;
-    } else {
-      return this.previewImage.src || '';
-    }
+const emit = defineEmits<{
+  'uploaded-image-src': [src: string];
+  'update:image': [file: File];
+}>();
+
+const instance = getCurrentInstance();
+const $axios = instance?.appContext.config.globalProperties.$axios;
+const store = useStore();
+
+const image = ref<File | null>(null);
+const uploadedImageSrc = ref<any>('');
+const uploading = ref(false);
+const imageUploader = ref();
+
+const uploadedImage = computed(() => {
+  if (uploadedImageSrc.value) {
+    return uploadedImageSrc.value;
+  } else {
+    return props.previewImage.src || '';
   }
+});
 
-  set uploadedImage(src) {
-    this.previewImage.src = src;
-  }
-
-  private selectImage() {
-    const fileUploader = this.$refs.imageUploader as Vue;
-
-    const fileInput = fileUploader.$refs.input as HTMLInputElement;
+const selectImage = () => {
+  const fileInput = imageUploader.value?.$refs?.input as HTMLInputElement;
+  if (fileInput) {
     fileInput.click();
   }
+};
 
-  private async imageUploaded(file: File) {
-    if (file) {
-      this.image = file;
+const imageUploaded = async (file: File) => {
+  if (file) {
+    image.value = file;
 
-      const reader = new FileReader();
+    const reader = new FileReader();
 
-      reader.addEventListener('load', async e => {
-        this.uploadedImageSrc = e.target!.result as string;
-        this.$emit('uploaded-image-src', this.uploadedImageSrc);
+    reader.addEventListener('load', async (e) => {
+      uploadedImageSrc.value = e.target!.result as string;
+      emit('uploaded-image-src', uploadedImageSrc.value);
+    });
 
-        // reader.readAsDataURL(file);
+    reader.readAsDataURL(file);
+    emit('update:image', file);
 
-        // this.$emit('update:image', image);
-      });
-
-      reader.readAsDataURL(file);
-      this.$emit('update:image', file);
-
-      // TODO: add it back but only for image posts.
-
-      // const image = await compressAccurately(file, 500);
-
-      // const image = await compress(file, 0.5);
-
-      // console.log('Image =>', image);
-
-      // this.$emit('update:image', image);
-    }
+    // TODO: add it back but only for image posts.
+    // const image = await compressAccurately(file, 500);
+    // const image = await compress(file, 0.5);
+    // console.log('Image =>', image);
+    // emit('update:image', image);
   }
+};
 
-  private upload() {
-    const headers = {
-      'Content-Type': 'multipart/form-data',
-    };
+const upload = () => {
+  const headers = {
+    'Content-Type': 'multipart/form-data',
+  };
 
-    this.uploading = true;
+  uploading.value = true;
 
-    const formData = new FormData();
+  const formData = new FormData();
+  formData.append('image', image.value as File);
 
-    formData.append('image', this.image as File);
+  $axios
+    .post(
+      `/files/upload?filename=${props.fileName}&filepath=${props.filePath}`,
+      formData,
+      {
+        headers,
+      }
+    )
+    .then((response: any) => {
+      console.log('res => ', response.data);
+    })
+    .catch((response: any) => {
+      console.log('res => ', response.data);
 
-    this.$axios
-      .post(
-        `/files/upload?filename=${this.fileName}&filepath=${this.filePath}`,
-        formData,
-        {
-          headers,
-        }
-      )
-      .then(response => {
-        console.log('res => ', response.data);
-
-        // this.image = undefined;
-
-        // this.uploadedImage = '';
-      })
-      .catch(response => {
-        console.log('res => ', response.data);
-
-        this.$store.dispatch('SHOW_TOAST', {
-          message: 'Error uploading image',
-          style: 'error',
-        });
-      })
-      .finally(() => {
-        this.uploading = false;
-        this.image = null;
+      store.showToast({
+        message: 'Error uploading image',
+        style: 'error',
       });
-  }
-}
+    })
+    .finally(() => {
+      uploading.value = false;
+      image.value = null;
+    });
+};
 </script>
 
 <style></style>
