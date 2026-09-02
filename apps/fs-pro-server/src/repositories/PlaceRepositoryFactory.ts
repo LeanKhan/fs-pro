05051d14b2ca3851abfd9cd09b3848fc980d2e43
Emmanuel Segun-Lean <34924520+LeanKhan@users.noreply.github.com>
2026-09-02 09:10:33 -0500

    chore: switch eslint for oxlint

--- apps/fs-pro-server/src/repositories/PlaceRepositoryFactory.ts
+++ apps/fs-pro-server/src/repositories/PlaceRepositoryFactory.ts
@@ -1,6 +1,6 @@
-import { IPlaceRepository } from './PlaceRepository';
-import { DrizzlePlaceRepository } from './drizzle/PlaceRepository';
-import { DrizzleDatabase } from '../db/drizzle';
+import { IPlaceRepository } from "./PlaceRepository";
+import { DrizzlePlaceRepository } from "./drizzle/PlaceRepository";
+import { DrizzleDatabase } from "../db/drizzle";
 
 export class PlaceRepositoryFactory {
   static create(): IPlaceRepository {
